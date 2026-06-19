86af69861fcb0e0b5ef2205d6e73c5cfd01e8e47
Oriana Carotti <ocarotti@obralapiedad.com.ar>
2026-06-19 16:12:45 -0300

    Add files via upload

---
+++ Batalla.java
@@ -0,0 +1,24 @@
+
+import java.util.Scanner;
+
+public class Batalla{
+    Scanner sc = new Scanner(System.in);
+
+    Pokemon pkn = new Pokemon();
+
+    int opcion;
+    do{
+        System.out.println("");
+        System.out.println("1.Evolucionar pokémon");
+        System.out.println("2.Batalla");
+        System.out.println("3.Salir");
+        
+        System.out.println("Seleccione una opción: ");
+
+        switch(opcion){
+            case 1:
+            case 2:
+                
+        }
+    }
+}
