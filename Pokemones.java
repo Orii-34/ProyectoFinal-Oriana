c0937fa04f93d24902ed889e0afb3262279e4899
Oriana Carotti <ocarotti@obralapiedad.com.ar>
2026-06-19 16:14:00 -0300

    Add files via upload

---
+++ Pokemones.java
@@ -0,0 +1,23 @@
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Tipos{
+    
+    ArrayList<Pokemon> pokemones = new ArrayList<>();
+    Scanner sc = new Scanner(System.in);
+
+    nombre1 = sc.next();
+    tipo1 = sc.next();
+    vida1 = sc.next();
+    nivel1 = sc.next();
+    nivAmistad1 = sc.next();
+    estadistica1 = sc.next();
+
+    nombre2 = sc.next();
+    tipo2 = sc.next();
+    vida2 = sc.next();
+    nivel2 = sc.next();
+    nivAmistad2 = sc.next();
+    estadistica2 = sc.next();
+}
